e2440959305971aa48a632150fe0266fe0455678
isaac <isaac.code980@gmail.com>
2023-03-14 14:10:53 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Create two variables, firstName and lastName
+let firstName = "John"
+let lastName = "Doe"
+
+// Concatenate the two variables into a third variable called fullName
+let fullName = `${firstName} ${lastName}`
+
+// Log fullName to the console
+console.log(fullName) // John Doe
